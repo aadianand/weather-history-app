478cb4671d15a8692139e60250cbc10a828ba01a
Aditya Anand <adityaanand20122002@gmail.com>
2025-05-29 09:36:09 +0530

    fix: suppress unused variable warnings in charts.tsx to resolve Vercel build failure

--- components/ui/charts.tsx
+++ components/ui/charts.tsx
@@ -98,7 +98,8 @@ const ChartLegendContent = React.forwardRef<
     }
 >(({ className, hideIcon = false, payload, verticalAlign = "bottom", nameKey }, ref) => {
   // TODO: Fix this
-  const _config = {}
+ // eslint-disable-next-line @typescript-eslint/no-unused-vars
+const _config = {}
   // const { config } = useChart()
 
   if (!payload?.length) {
@@ -111,6 +112,7 @@ const ChartLegendContent = React.forwardRef<
       className={cn("flex items-center justify-center gap-4", verticalAlign === "top" ? "pb-3" : "pt-3", className)}
     >
       {payload.map((item) => {
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
         const _key = `${nameKey || item.dataKey || "value"}` 
         // const itemConfig = getPayloadConfigFromPayload(config, item, key)
         const itemConfig = {
